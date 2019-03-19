1a2100a67834ec59b6ed426252415ecb72e3d27d
Laurel Butler <laurelbutler@Laurels-MBP.attlocal.net>
2019-03-19 11:43:17 -0400

    tried some testing

--- trelloyes-testing/src/App.test.js
+++ trelloyes-testing/src/App.test.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
+  const div = document.createElement('div'); //must be written with every smoke test
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
