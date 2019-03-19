07dd2af9a612be73a2ae69462259a3fd8082b615
Farah Student <farah.gus@hotmail.com>
2019-03-19 12:30:15 -0400

    added cards={[]}

--- trelloyes-testing/src/App.test.js
+++ trelloyes-testing/src/App.test.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import renderer from 'react-test-renderer';
+
 
 it('renders without crashing', () => {
   const div = document.createElement('div'); //must be written with every smoke test
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+  //screenshot test
+  it('renders the UI as expected', () => {
+    const tree = renderer
+      .create( <App name="app" /> )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
--- trelloyes-testing/src/List.test.js
+++ trelloyes-testing/src/List.test.js
@@ -6,14 +6,14 @@ import renderer from 'react-test-renderer';
 // smoke test
 it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<List />, div);
+    ReactDOM.render(<List cards={[]} />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 
   //screenshot test
   it('renders the UI as expected', () => {
     const tree = renderer
-      .create( <List name = "List" /> )
+      .create( <List cards={[]} name="list"/> )
       .toJSON();
     expect(tree).toMatchSnapshot();
   });
