fcd8ffa45e97ca90cd4b5fc6e0c966a100fd4318
Farah Student <farah.gus@hotmail.com>
2019-03-19 11:36:54 -0400

    snapshot test

--- trelloyes-testing/src/Card.test.js
+++ trelloyes-testing/src/Card.test.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import Card from './Card';
 import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
 
 // smoke test
+
 it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Card />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
+
+// snapshot test
+it ('renders the UI as expected', () => {
+    const tree = renderer
+        .create(<Card name="Card" />)
+        .toJSON();
+    expect(tree).toMatchSnapshot();
+});
