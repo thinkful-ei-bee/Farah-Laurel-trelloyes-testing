db7338146472e8a0fa6784c92394e04ae3e2ff49
Laurel Butler <laurelbutler@Laurels-MBP.attlocal.net>
2019-03-19 11:57:35 -0400

    debugging failed tests

--- trelloyes-testing/src/List.test.js
+++ trelloyes-testing/src/List.test.js
@@ -1,10 +1,19 @@
 import React from 'react';
-import Card from './Card';
+import List from './List';
 import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
 
 // smoke test
 it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Card />, div);
+    ReactDOM.render(<List />, div);
     ReactDOM.unmountComponentAtNode(div);
+  });
+
+  //screenshot test
+  it('renders the UI as expected', () => {
+    const tree = renderer
+      .create( <List name = "List" /> )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
   });
